c3ba1299348a72b21337790ef11170390c6008eb
Trevor L Davis <trevor.l.davis@gmail.com>
2022-11-09 22:10:02 +0000

    version 1.0.1

---
+++ vignettes/gganimate.R
@@ -0,0 +1,9 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>",
+  dev = "ragg_png",
+  fig.width = 6,
+  fig.height = 4
+)
+
